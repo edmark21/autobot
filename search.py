6aad4a435b8b725113ef8aa98f13c10a80d9c96c
Mr robot <30386159+edmark21@users.noreply.github.com>
2021-08-11 02:29:34 +0800

    Update search.py

--- search.py
+++ search.py
@@ -1,9 +1,9 @@
 
 import json,urllib.request
-import os, sys
+import os, sys, requests
 os.system('clear')
 lo = '''\033[1;33m
-                    Version 1
+                    V1
         
 
       ___  _   _ _____ ___________  _____ _____ 
@@ -25,27 +25,36 @@ lo = '''\033[1;33m
 '''
 print(lo)
 #def acc():
-	
+#c = "WS77-JZH3-PNY1"
 #c = input("\33[34mEnter code: ")
 #os.system('clear')
 def main():
-  c = input("Enter code: ")
+  c = "WS77-JNB1-FSY4"
   for x in range(20):
     print("\x1b[34m\ncredits: ", x ,"/ 20")
+    
     n = input("\033[1;33mEnter number#: ")
     data = urllib.request.urlopen("https://trial.serviceobjects.com/gppl2/api.svc/PhoneInfo/"+n+"/full/"+c+"?format=json").read()
+  
     output = json.loads(data)
+
     
-    d = (output['PhoneInfo']['Provider'])
-    dd = (output['PhoneInfo']['Contacts'])
-    print ('\033[32m\nName: '+dd[0]['Name'])
-    print ("Steet: "+dd[0]['Address'])
-    print ("City: "+dd[0]['City'])
-    print ("State: "+dd[0]['State'])
-    main()
+    try:
+    	dd = (output['PhoneInfo']['Contacts'])
+    	print ('\033[32m\nName: '+dd[0]['Name'])
+    	print ("Steet: "+dd[0]['Address'])
+    	print ("City: "+dd[0]['City'])
+    	print ("State: "+dd[0]['State'])
+    	print("Address coordinates: " + dd[0]['Latitude'], dd[0]['Longitude'])
+    	
+    except:
+    	print("code is expired")
+    	break
+    	
+    
+    	  
     
 main()
 
 
-#WS77-RMB3-HQP1 
-
+ 
